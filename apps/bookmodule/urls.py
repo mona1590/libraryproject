5a3ef78badec98e1b0a75a991990df37e44baa2c
mona1590 <mon.a.a.a133555@gmail.com>
2026-03-31 23:00:27 +0300

    Lab 5

--- apps/bookmodule/urls.py
+++ apps/bookmodule/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path # type: ignore
 from . import views
 
+
 urlpatterns = [
-    path('', views.index, name="books.index"),
-    path('list_books/', views.list_books, name="books.list_books"),
-    path('<int:bookId>/', views.viewbook, name="books.view_one_book"),
-    path('aboutus/', views.aboutus, name="books.aboutus"),
+    path('html5/links', views.links),
+    path('html5/text/formatting', views.formatting),
+    path('html5/listing', views.listing),
+    path('html5/tables', views.tables),
 ]
